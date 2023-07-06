e5ed8b4e30999784eb189ffe4d5e7ceab61aa813
Stephani Sara Miler <107444034+StephaniMiler@users.noreply.github.com>
2023-07-06 09:17:07 -0300

    Add files via upload

---
+++ Exercícios/Ex95.c
@@ -0,0 +1,21 @@
+/*95- Leia um vetor de 10 posições e atribua o valor 0 para todos os elementos que possuírem valores negativos*/
+
+#include <stdio.h>
+int main(){
+
+    int vetor[10];
+
+    for(int i= 0; i<=9; i++){
+        printf("Digite o valor da %d posicao: ", i + 1);
+        scanf("%d", &vetor[i]);
+        if(vetor[i] < 0){
+            vetor[i] = 0;
+        }
+    }
+
+    for(int j= 0; j <=9; j++){
+        printf("%d\n", vetor[j]);
+    }
+
+    return 0;
+}
