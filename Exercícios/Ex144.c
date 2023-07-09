577367d19c366e488f5eff4707fe485e1e1160e5
Stephani Sara Miler <107444034+StephaniMiler@users.noreply.github.com>
2023-07-09 09:58:03 -0300

    Add files via upload

---
+++ Exercícios/Ex144.c
@@ -0,0 +1,33 @@
+/*144- Escreva um único programa que realize os seguinte passos:
+-Leia uma cadeia de caracteres do teclado, de no máximo 60 caracteres “úteis” e armazene em uma variável. Atenção, essa 
+string pode conter espaços em branco.
+-Calcule, armazene em uma variável e imprima o número de caracteres dessa string.
+Imprima na tela os caracteres da string na forma inversa. 
+Ex: 
+Entrada: 
+abacaxi. 
+saida: 7
+ixacaba.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char frase[61];
+    int tamanho;
+
+    printf("Digite uma frase: \n");
+    fgets(frase, 61, stdin);
+    
+    tamanho = strlen(frase) -1;
+
+    printf("%i\n", tamanho);
+    for(int i= tamanho; i>=0; i--){
+        printf("%c", frase[i]);
+    }
+
+    return 0;
+    
+}
